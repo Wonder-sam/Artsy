a43f0f0435e9057e482096f5ae7b363f457cdd0b
wonder <samuel.nai@yahoo.com>
2022-12-05 23:30:50 +0000

    add dummy data

---
+++ src/dataset/productsData.js
@@ -0,0 +1,83 @@
+import boolean from "../assets/images/boolean.svg"
+import egypt from "../assets/images/egypt.svg"
+import egyptian from "../assets/images/egyptians.svg"
+import creator1 from "../assets/images/Ellipse18.svg"
+import creator2 from "../assets/images/Ellipse19.svg"
+import creator3 from "../assets/images/Ellipse20.svg"
+import creator4 from "../assets/images/Ellipse21.svg"
+import creator5 from "../assets/images/Ellipse22.svg"
+
+export const DisplayProducts = [
+    {
+        id: 0,
+        productName: "The Boolean Egyptian",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus"+
+        "venenatis, lectus magna fringilla urna, porttitor rhoncus dolor pur",
+        image: boolean,
+        creators: [
+            {
+                image: creator1
+            },
+            {
+                image: creator2
+            },
+            {
+                image: creator3
+            },
+            {
+                image: creator4
+            },
+            {
+                image: creator5
+            }
+        ]
+    },
+    {
+        id: 1,
+        productName: "The Boolean Egyptian",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus"+
+        "venenatis, lectus magna fringilla urna, porttitor rhoncus dolor pur",
+        image: egyptian,
+        creators: [
+            {
+                image: creator2
+            },
+            {
+                image: creator3
+            },
+            {
+                image: creator4
+            },
+            {
+                image: creator5
+            },
+            {
+                image: creator1
+            }
+        ]
+    },
+    {
+        id: 2,
+        productName: "The Boolean Egyptian",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus"+
+        "venenatis, lectus magna fringilla urna, porttitor rhoncus dolor pur",
+        image: egypt,
+        creators: [
+            {
+                image: creator1
+            },
+            {
+                image: creator2
+            },
+            {
+                image: creator3
+            },
+            {
+                image: creator4
+            },
+            {
+                image: creator5
+            }
+        ]
+    }
+]

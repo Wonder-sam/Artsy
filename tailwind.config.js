b033070ebd9d67348da1091e7265402b9af73e2d
wonder <samuel.nai@yahoo.com>
2022-12-06 23:31:22 +0000

    add custom animations to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
 	"./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': {
+            opacity: 0
+          },
+          '100%': {
+            opacity: 1
+          }
+        },
+        fadeOut:{
+          '0%': {
+            opacity: 1
+          },
+          '100%': {
+            opacity: 0
+          }
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in 1',
+        fadeOut: 'fadeOut  1s ease-in 1'
+      }
+    },
   },
   plugins: [],
 }

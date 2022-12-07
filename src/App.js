a53c1964d9f034be8d4172d44215829ae8390bbe
wonder <samuel.nai@yahoo.com>
2022-12-07 01:12:27 +0000

    set initial route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, redirect } from 'react-router-dom';
 import Home from './pages/Home';
+import Header from './components/Header';
 
 const router = createBrowserRouter([
       {
         path: "/",
-        element: <Home />
+        element: <Header />,
+        
+        children: [
+          {
+            index: true,
+            element: <Home />,
+            loader: ()=>redirect('/Home'),
+          },
+          {
+            path: "Home",
+            element: <Home />
+          }
+        ]
       }
     ])
 

81635faad4d6cbe2de8632f3f37a58213daaa03d
wonder <samuel.nai@yahoo.com>
2022-12-07 23:55:30 +0000

    add dummy data for marketplace

---
+++ src/dataset/marketData.js
@@ -0,0 +1,67 @@
+import phill from './../assets/images/girlMoon.svg'
+import boolean from './../assets/images/boolean.svg'
+import girlpink from './../assets/images/girlpink.svg'
+import ellipsia from './../assets/images/ellipsian.svg'
+import lawmaker from './../assets/images/lawmaker.svg'
+import veil from './../assets/images/viel.svg'
+import alternating from './../assets/images/alternating.svg'
+import rosemary from './../assets/images/rosemary.svg'
+import beverly from './../assets/images/beverly.svg'
+
+
+export const MarketData = [
+    {
+        id: 0,
+        name: "Philomena '22",
+        image: phill,
+        price: 3.9
+    },
+    {
+        id: 1,
+        name: "Boolean Egyptian",
+        image: boolean,
+        price: 3.9
+    },
+    {
+        id: 2,
+        name: "Blanc",
+        image: girlpink,
+        price: 3.9
+    },
+    {
+        id: 3,
+        name: "Ellipsia",
+        image: ellipsia,
+        price: 3.9
+    },
+    {
+        id: 4,
+        name: "The Lawmakers",
+        image: lawmaker,
+        price: 3.9
+    },
+    {
+        id: 5,
+        name: "Veil",
+        image: veil,
+        price: 3.9
+    },
+    {
+        id: 6,
+        name: "Alternating",
+        image: alternating,
+        price: 3.9
+    },
+    {
+        id: 7,
+        name: "Rosemary '22",
+        image: rosemary,
+        price: 3.9
+    },
+    {
+        id: 8,
+        name: "Beverly",
+        image: beverly,
+        price: 3.9
+    },
+]

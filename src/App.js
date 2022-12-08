fb5f7653df650c982ac8a784fef62049ad8fb2c6
wonder <samuel.nai@yahoo.com>
2022-12-08 23:33:07 +0000

    add loader to routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import { createBrowserRouter, redirect } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
 import { Marketplace } from './pages/MarketPlace';
+import { MarketData } from './dataset/marketData';
+import Product from './pages/Product';
+
+async function getProducts() {
+  return MarketData
+}
+async function getProduct(num) {
+  return MarketData[num]
+}
 
 const router = createBrowserRouter([
       {
@@ -18,12 +27,19 @@ const router = createBrowserRouter([
           },
           {
             path: "Home",
-            element: <Home />
+            element: <Home />,
           },
           {
             path: "Marketplace",
-            element: <Marketplace />
+            element: <Marketplace />,
+            loader: getProducts
+          },
+          {
+            path: "Marketplace/:productId",
+            element: <Product />,
+            loader: ({params})=> getProduct(params.productId)
           }
+
         ]
       }
     ])

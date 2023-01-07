3f00255dbe10d52ce06eef361dc49bf8a8219fef
wonder <samuel.nai@yahoo.com>
2023-01-07 15:33:25 +0000

    configure routes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import Header from './components/Header';
 import { Marketplace } from './pages/MarketPlace';
 import { MarketData } from './dataset/marketData';
 import Product from './pages/Product';
+import Auction from './pages/Auction';
+import LiveBid from './pages/LiveBid';
+import { auctionData } from './dataset/auctionData';
 
 async function getProducts() {
   return MarketData
 }
 async function getProduct(num) {
   return MarketData[num]
+}
+
+async function getAuction(num) {
+  return auctionData[num]
 }
 
 const router = createBrowserRouter([
@@ -39,7 +46,16 @@ const router = createBrowserRouter([
             path: "Marketplace/:productId",
             element: <Product />,
             loader: ({params})=> getProduct(params.productId)
-          }
+          },
+          {
+            path: "Auctions",
+            element: <Auction />
+          },
+          {
+            path: "Auctions/:auctionId",
+            element: <LiveBid />,
+            loader: ({params})=> getAuction(params.auctionId)
+          },
 
         ]
       }
